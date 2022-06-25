7dd32c0ae1d8e9a77c3150a086abe33d3e49b721
macheky <junot044@gmail.com>
2022-06-25 15:32:47 +0300

    upload first time my mvola app with symfony

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,13 +6,15 @@ use Faker\Factory;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
 
     private $encoder;
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    // public function __construct(UserPasswordEncoderInterface $encoder)
+    public function __construct(UserPasswordHasherInterface $encoder)
     {
         $this->encoder = $encoder;
     }
@@ -30,7 +32,11 @@ class AppFixtures extends Fixture
             ->setPassword("adminpw")
             ->setRoles(["ROLE_ADMIN"]);
 
-        $user->setPassword($this->encoder->encodePassWord($user, $user->getPassword()));
+        // $user->setPassword($this->encoder->encodePassWord($user, $user->getPassword()));
+        $user->setPassword($this->encoder->hashPassword(
+            $user,
+            $user->getPassword()
+        ));
 
         $manager->persist($user);
 
